28e0f2b8084bd1f99c47a39ab010b4da10b4cfeb
Panichaptmw <68456772+Panichaptmw@users.noreply.github.com>
2020-07-18 22:00:43 +0700

    Add files via upload

---
+++ homework_1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	cout << "1st Term of 2020\t\t" << "\\6306021620166\t" << "Panicha Pattamawarawong" << endl;
+	cout << endl;
+	cout << "------------------------------------------------------------------------------------------------" << endl;
+	cout << "| Subject ID | Subject Name\t\t" << "| Score (100)\t| " "| Grade | " << endl;
+	cout << "------------------------------------------------------------------------------------------------" << endl;
+	cout << "|  4570034  | " << "Introduction to computer\t" << "|\t85\t|" << " |  \"A\"  |" << endl;
+	cout << "|  4580247  | " << "English\t\t\t" << "|\t74\t|" << " |  \"B\"  |" << endl;
+	cout << "|  4580247  | " << "Design Thinking\t\t" << "|\t92\t|" << " |  \"A\"  |" << endl;
+	cout << "------------------------------------------------------------------------------------------------" << endl;
+	cout << "|   Total   | " << "\tSubject = 3\t\t" << "| Average = 3.87| " << endl;
+	cout << "================================================================================================" << endl;
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	cout << "KMUTNB" << "\n" << endl;
+	return 0;
+}
